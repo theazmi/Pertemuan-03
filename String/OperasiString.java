4cc6605f60adb4547f0fe4d96eb3cb2c12811120
theazmi <133631126+theazmi@users.noreply.github.com>
2023-05-15 17:07:51 +0800

    Initial commit

---
+++ String/OperasiString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package String;
+
+/**
+ *
+ * @author Axioo Class Program
+ */
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Banjarbaru";
+        
+        System.out.println(kota);
+        char
+    }
+}
